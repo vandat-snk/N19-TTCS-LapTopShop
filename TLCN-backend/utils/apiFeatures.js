4f43cbd4514b424021165fe1af154c87b814cbea
vandat-snk <datlamson2005@gmail.com>
2026-05-21 13:18:10 +0700

    Backup apiFeatures local before merge Huy

--- TLCN-backend/utils/apiFeatures.js
+++ TLCN-backend/utils/apiFeatures.js
@@ -6,26 +6,64 @@ class APIFeatures {
 
   filter() {
     const queryObj = { ...this.queryString };
+
+    // Các field không dùng để lọc
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
-    Object.keys(queryObj).forEach(function (key) {
-      if (key != "price" && key != "promotion") {
-        if (key != "keyword") {
-          queryObj[key] = { $in: queryObj[key].split(",") };
-        }
-        if ((key == "keyword")) {
-          queryObj["$text"] = { $search: queryObj[key] };
-          delete queryObj[key];
-        }
+
+    // Map query trên URL sang đúng field trong MongoDB
+    // Ví dụ: ?ram=16GB -> specs.ram
+    const specsFieldMap = {
+      ram: "specs.ram",
+      color: "specs.color",
+      cpu: "specs.cpu",
+      os: "specs.os",
+      weight: "specs.weight",
+      screen: "specs.screen",
+      graphicCard: "specs.graphicCard",
+      storage: "specs.storage",
+      battery: "specs.battery",
+      demand: "specs.demand",
+    };
+
+    Object.keys(queryObj).forEach((key) => {
+      // Tìm kiếm text
+      if (key === "keyword") {
+        queryObj["$text"] = { $search: queryObj[key] };
+        delete queryObj[key];
+        return;
       }
+
+      // Nếu là field thuộc specs thì đổi key
+      if (specsFieldMap[key]) {
+        queryObj[specsFieldMap[key]] = {
+          $in: queryObj[key].split(","),
+        };
+        delete queryObj[key];
+        return;
+      }
+
+      // Các field dạng khoảng giá: price, promotion
+      // Giữ nguyên để xử lý gte, lte, gt, lt ở dưới
+      if (key === "price" || key === "promotion") {
+        return;
+      }
+
+      // Các field lọc bình thường như brand, category
+      queryObj[key] = {
+        $in: queryObj[key].split(","),
+      };
     });
-    // console.log(queryObj);
-    // queryObj.brand = { $in: queryObj.brand.split(",") };
-    // Advanced filtering
+
+    // Advanced filtering: price[gte], price[lte], promotion[lt]...
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
 
     this.query = this.query.find(JSON.parse(queryStr));
+
     return this;
   }
 
